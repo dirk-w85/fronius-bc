e7b9c089920aaf64e77f5b505891887ec4d7d5ce
Dirk Woellhaf <dirk.woellhaf@gmail.com>
2025-08-01 18:43:12 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 echo "Starting..."
-go run -ldflags "-X main.curVersion=$(git describe --always --long) -X 'main.curBuild=$(date)'" main.go
+podman run --rm \
+-v $PWD:/app docker.io/dirkw85/dev-golang:latest \
+go run -ldflags "-X main.curVersion=$(git rev-list --count HEAD)-$(git describe --always --long) -X 'main.curBuild=$(date)'" main.go
